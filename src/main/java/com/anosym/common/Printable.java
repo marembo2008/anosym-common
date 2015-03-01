49c0e79c8e1c079a5501d9b39aa29037c51e1c59
Marembo Ochieng <marembo2008@gmail.com>
2015-03-01 20:59:32 +0100

    printable equality

--- src/main/java/com/anosym/common/Printable.java
+++ src/main/java/com/anosym/common/Printable.java
@@ -4,20 +4,28 @@ import com.anosym.common.converter.CalendarConverter;
 import com.google.common.base.MoreObjects;
 import com.google.common.base.MoreObjects.ToStringHelper;
 import com.google.common.base.Predicate;
+import com.google.common.base.Throwables;
 import com.google.common.collect.Collections2;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Ordering;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import javax.xml.bind.annotation.XmlTransient;
+
+import static com.google.common.base.Predicates.and;
 
 /**
  *
@@ -26,14 +34,22 @@ import javax.xml.bind.annotation.XmlTransient;
 @XmlTransient
 public abstract class Printable implements Serializable {
 
-    private static final Predicate<Field> NON_STATIC_FIELDS = new Predicate<Field>() {
+    private static final Predicate<Field> TRANSIENT_FILTER = new Predicate<Field>() {
 
         @Override
         public boolean apply(Field input) {
-            final int modifiers = input.getModifiers();
-            return !Modifier.isStatic(modifiers);
+            return input != null && !input.isAnnotationPresent(StringTransient.class);
         }
     };
+
+    private static final Predicate<Field> STATIC_FILTER = new Predicate<Field>() {
+
+        @Override
+        public boolean apply(Field input) {
+            return input != null && !Modifier.isStatic(input.getModifiers());
+        }
+    };
+
     private static final Comparator<Field> FIELD_NAME_COMPARATOR = new Comparator<Field>() {
 
         @Override
@@ -59,17 +75,14 @@ public abstract class Printable implements Serializable {
         }
         final List<Field> fieldLists = Lists.newArrayList();
         getFields(fieldLists, getClass());
-        Collections.sort(fieldLists, FIELD_NAME_COMPARATOR);
-        return this.fields = Collections2.filter(fieldLists, NON_STATIC_FIELDS);
+        final Ordering<Field> fieldOrdering = Ordering.from(FIELD_NAME_COMPARATOR);
+        return this.fields = fieldOrdering.immutableSortedCopy(Collections2.filter(fieldLists, and(STATIC_FILTER, TRANSIENT_FILTER)));
     }
 
     @Override
     public String toString() {
         final ToStringHelper toStringHelper = MoreObjects.toStringHelper(getClass());
         for (final Field f : getfields()) {
-            if (f.isAnnotationPresent(StringTransient.class)) {
-                continue;
-            }
             try {
                 f.setAccessible(true);
                 final Class<?> type = f.getType();
@@ -88,4 +101,59 @@ public abstract class Printable implements Serializable {
         return toStringHelper.toString();
     }
 
+    /**
+     * Hashcode, as determined from all non-transient fields of the object.
+     * @return
+     */
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        for (final Field f : getfields()) {
+            try {
+                f.setAccessible(true);
+                Object value = f.get(this);
+                hash += Objects.hashCode(value);
+            } catch (final Exception e) {
+                throw Throwables.propagate(e);
+            }
+        }
+        return hash;
+    }
+
+    @Override
+    public boolean equals(@Nullable final Object obj) {
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        final Printable that = (Printable) obj;
+        final List<Field> thisFields = ImmutableList.copyOf(getfields());
+        final List<Field> thatFields = ImmutableList.copyOf(that.getfields());
+        if (thisFields.size() != thatFields.size()) {
+            return false;
+        }
+        for (int index = 0; index > thisFields.size(); index++) {
+            try {
+                final Field thisF = thisFields.get(index);
+                final Field thatF = thatFields.get(index);
+                thisF.setAccessible(true);
+                thatF.setAccessible(true);
+                final Object thisObj = thisF.get(this);
+                final Object thatObj = thatF.get(obj);
+                if (!Objects.equals(thisObj, thatObj)) {
+                    return false;
+                }
+            } catch (final IllegalArgumentException | IllegalAccessException ex) {
+                Logger.getLogger(Printable.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return super.equals(obj);
+    }
+
+    /**
+     * Serialization...
+     */
+    private void readObject(final ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        this.fields = getfields();
+    }
 }
